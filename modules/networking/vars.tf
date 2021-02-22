75c3659440fc6d079752149e1e5e5426ae9053d9
burhan <burhanshaikh87@gmail.com>
2021-02-22 10:39:29 +0530

    networking variables

---
+++ modules/networking/vars.tf
@@ -0,0 +1,44 @@
+variable "vpccidr" {
+
+    default = "10.1.0.0/16"
+}
+
+variable "subnet1cidr" {
+
+    default = "10.1.1.0/24"
+}
+
+variable "subnet2cidr" {
+
+    default = "10.1.2.0/24"
+}
+
+variable "subnet3cidr" {
+
+    default = "10.1.3.0/24"
+}
+
+variable "subnet4cidr" {
+
+    default = "10.1.4.0/24"
+}
+
+variable "subnet5cidr" {
+
+    default = "10.1.5.0/24"
+}
+
+variable "subnet6cidr" {
+
+    default = "10.1.6.0/24"
+}
+
+variable "availzone1" {
+
+    default = "us-east-1a"
+}
+
+variable "availzone2" {
+
+    default = "us-east-1b"
+}
