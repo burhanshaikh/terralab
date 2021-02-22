d9209bd3414f0ec4c26a3a296b4ea7d1480077f3
burhan <burhanshaikh87@gmail.com>
2021-02-22 12:01:40 +0530

    Database Module Variables Added

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -1,13 +1,12 @@
 resource "aws_db_instance" "postgres" {
-  allocated_storage    = 5
-  storage_type         = "standard"
-  engine               = "postgres"
-  //engine_version       = "12.5"
-  instance_class       = "db.t2.micro"
-  name                 = "mypostgresdatabase"
-  username             = "testdb"
-  password             = "8characters"
-  skip_final_snapshot = true
+  allocated_storage    = var.allocated_storage
+  storage_type         = var.storage_type
+  engine               = var.engine
+  instance_class       = var.instance_class
+  name                 = var.name
+  username             = var.username
+  password             = var.password
+  skip_final_snapshot  = var.skip_final_snapshot
   vpc_security_group_ids = var.vpc_security_group_ids
   db_subnet_group_name = var.db_subnet_group_name
   //security_group_names = [module.networking.securitygroupname]
--- modules/database/vars.tf
+++ modules/database/vars.tf
@@ -5,3 +5,37 @@ variable "vpc_security_group_ids" {
 variable "db_subnet_group_name" {
   description = "DB subnet group name that contains two subnets in different AZ's"
 }
+
+variable "allocated_storage" {
+  default = 5
+}
+
+variable "storage_type" {
+  default = "standard"
+}
+
+variable "engine" {
+  default = "postgres"
+}
+
+variable "instance_class" {
+  default = "db.t2.micro"
+}
+
+variable "name" {
+  default = "mypostgresdatabase"
+}
+variable "username" {
+  default = "testdb"
+  sensitive = true
+}
+
+variable "password" {
+  default = "8characters"
+  sensitive = true
+}
+
+variable "skip_final_snapshot" {
+  type = bool
+  default = true
+}
